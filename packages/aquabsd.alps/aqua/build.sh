17f85a89adefda59d4838b76c3baec98b84c4274
obiwac <obiwac@gmail.com>
2021-10-20 17:15:36 +0200

    Finished `aquabsd.alps/aqua` package

--- packages/aquabsd.alps/aqua/build.sh
+++ packages/aquabsd.alps/aqua/build.sh
@@ -1,13 +1,37 @@
 #!/bin/sh
 set -e
 
-# build the package files
+BUILD_DIR=".build/"
 
-rm -rf .build/
-mkdir .build/
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR/package/usr/
+
+( cd $BUILD_DIR
+	# build the package files
+
+	git clone https://github.com/inobulles/aqua-unix --branch main --depth 1
+	
+	( cd aqua-unix/
+		sh build.sh --kos --devices --devbranch aquabsd.alps
+	)
+	
+	# create package directories
+
+	mkdir package/usr/bin/
+	mkdir -p package/usr/share/aqua/devices/
+
+	# move built files to their appropriate places in the package
+
+	mv aqua-unix/bin/kos package/usr/bin/aqua
+	mv aqua-unix/bin/devices/* package/usr/share/aqua/devices/
+	
+	# the 'aquabsd.alps.ui' device is not (yet) open source, so create a stub for it
+
+	touch package/usr/share/aqua/devices/aquabsd.alps.ui.device
+)
 
 # create the package tarball
 
-pkg create -M manifest.json -p plist -r .build
+pkg create -M manifest.json -p plist -r $BUILD_DIR/package
 
 exit 0
