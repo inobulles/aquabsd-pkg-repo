7586b00af3c05fcaa0378d22972aaa4e5db034ea
obiwac <obiwac@gmail.com>
2022-02-15 00:48:43 +0100

    build: now categories to be built must be passed as command line arguments

--- build.sh
+++ build.sh
@@ -8,21 +8,33 @@ mkdir $BUILD_DIR
 
 BUILD_DIR=$(realpath $BUILD_DIR)
 
-# build all packages
+# go through each category (passed as arguments)
 
-( cd packages/
-	for category in $(find -L . -maxdepth 1 -type d -not -name ".*" | cut -c3-); do
-		echo "Building packages from the '$category' category ..."
+if [ $# = 0 ]; then
+	echo "You must specify one or more categories to build"
+	exit 1
+fi
 
-		( cd $category
-			for package in $(find -L . -maxdepth 1 -type d -not -name ".*" | cut -c3-); do
-				echo -e "\tBuilding '$category/$package' ..."
-				
-				( cd $package
-					sh build.sh
-					mv *.pkg $BUILD_DIR
-				)
-			done
-		) 
-	done
-)
+while test $# -gt 0; do
+	category="packages/$1"
+
+	if [ ! -d $category ]; then
+		echo "Can't find the $1 category 😢"
+		exit 1
+	fi
+
+	echo "Building packages from the '$1' category ..."
+
+	( cd $category
+		for package in $(find -L . -maxdepth 1 -type d -not -name ".*" | cut -c3-); do
+			echo -e "\tBuilding '$category/$package' ..."
+
+			( cd $package
+				sh build.sh
+				mv *.pkg $BUILD_DIR
+			)
+		done
+	)
+
+	shift
+done
