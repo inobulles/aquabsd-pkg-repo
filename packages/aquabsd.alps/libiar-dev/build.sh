0dea42560537d14859492bc5aad9e9df1e7d46ad
obiwac <obiwac@gmail.com>
2022-02-21 22:56:26 +0100

    aquabsd.alps: updated and pruned some packages

--- packages/aquabsd.alps/libiar-dev/build.sh
+++ packages/aquabsd.alps/libiar-dev/build.sh
@@ -7,22 +7,17 @@ rm -rf $BUILD_DIR
 mkdir -p $BUILD_DIR/package/usr/
 
 ( cd $BUILD_DIR
-	# build the package files
+	# get the package files
 
 	git clone https://github.com/inobulles/iar --branch main --depth 1
 
-	( cd iar/
-		# we actually need to install this completely on our system in order to link the 'libiar' shared library
-		sh build.sh _
-	)
-
 	# create package directories
 
-	mkdir package/usr/lib/
+	mkdir package/usr/include/
 
 	# move built files to their appropriate place in the package
 
-	mv iar/bin/libiar.a package/usr/lib/libiar.a
+	mv iar/src/iar.h package/usr/include/iar.h
 )
 
 # create the package tarball
